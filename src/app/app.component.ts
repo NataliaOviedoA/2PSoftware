7686afb67c475c0653f0b3c41b04155031366fcb
NataliaOviedoA <oviedoacostanatalia@gmail.com>
2019-05-27 11:28:40 -0400

    Añadiendo service workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pwa';
+export class AppComponent implements OnInit{
+  title = 'Mi app con angular';
+  constructor(private swUpdate:SwUpdate){
+
+  }
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled){
+      this.swUpdate.available.subscribe(()=>{
+        window.location.reload();
+      });
+    }
+  }
 }
